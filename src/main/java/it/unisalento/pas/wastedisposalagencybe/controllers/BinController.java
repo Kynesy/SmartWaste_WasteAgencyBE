4a7731b56aa0a2b70474473f053564987985279c
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-22 19:24:13 +0200

    creato bin controller

---
+++ src/main/java/it/unisalento/pas/wastedisposalagencybe/controllers/BinController.java
@@ -0,0 +1,136 @@
+package it.unisalento.pas.wastedisposalagencybe.controllers;
+
+import it.unisalento.pas.wastedisposalagencybe.domains.Bin;
+import it.unisalento.pas.wastedisposalagencybe.dto.BinDTO;
+import it.unisalento.pas.wastedisposalagencybe.services.IBinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/bin")
+public class BinController {
+    private final IBinService binService;
+
+    @Autowired
+    public BinController(IBinService binService) {
+        this.binService = binService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createBin(@RequestBody BinDTO binDTO){
+        Bin bin = fromBinDTOtoBin(binDTO);
+
+        int result = binService.createBin(bin);
+        if(result == 0){
+            return ResponseEntity.status(HttpStatus.OK).body("{\"message\": \"Bin created successfully\"}");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"message\": \"Bin creation failed\"}");
+        }
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<String> updateBin(@RequestBody BinDTO binDTO) {
+        Bin bin = fromBinDTOtoBin(binDTO);
+
+        int result = binService.updateBin(bin);
+        if (result == 0) {
+            return ResponseEntity.status(HttpStatus.OK).body("{\"message\": \"Bin updated successfully\"}");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"message\": \"Bin update failed\"}");
+        }
+    }
+
+    @PostMapping("/setWaste/{binID}/{sortedWaste}/{unsortedWaste}")
+    public ResponseEntity<String> setWaste(@PathVariable String binID,
+                                           @PathVariable int sortedWaste,
+                                           @PathVariable int unsortedWaste) {
+        int result = binService.addWaste(binID, sortedWaste, unsortedWaste);
+        if (result == 0) {
+            return ResponseEntity.status(HttpStatus.OK).body("{\"message\": \"Waste values updated successfully\"}");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"message\": \"Waste values update failed\"}");
+        }
+    }
+
+    @PostMapping("/unload/{binID}")
+    public ResponseEntity<String> unloadBin(@PathVariable String binID) {
+        int result = binService.unloadBin(binID);
+        if (result == 0) {
+            return ResponseEntity.status(HttpStatus.OK).body("{\"message\": \"Bin waste unloaded successfully\"}");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"message\": \"Bin waste unload failed\"}");
+        }
+    }
+
+
+
+    @DeleteMapping("/delete/{binID}")
+    public ResponseEntity<String> deleteBin(@PathVariable String binID) {
+        int result = binService.deleteBinByID(binID);
+        if (result == 0) {
+            return ResponseEntity.status(HttpStatus.OK).body("{\"message\": \"Bin deleted successfully\"}");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"message\": \"Bin deletion failed\"}");
+        }
+    }
+
+    @GetMapping("/get/{binID}")
+    public ResponseEntity<BinDTO> getBin(@PathVariable String binID) {
+        Bin bin = binService.getBinbyID(binID);
+
+        if (bin != null) {
+            BinDTO binDTO = fromBinToBinDTO(bin);
+            return ResponseEntity.ok(binDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<ArrayList<BinDTO>> getAllBins() {
+        ArrayList<Bin> binList = binService.getAllBins();
+        ArrayList<BinDTO> binDTOList = new ArrayList<>();
+        if (binList != null) {
+            for(Bin bin : binList){
+                BinDTO binDTO = fromBinToBinDTO(bin);
+                binDTOList.add(binDTO);
+            }
+            return ResponseEntity.ok(binDTOList);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    private BinDTO fromBinToBinDTO(Bin bin) {
+        BinDTO binDTO = new BinDTO();
+
+        binDTO.setId(bin.getId());
+        binDTO.setCapacity(bin.getCapacity());
+        binDTO.setLongitude(bin.getLongitude());
+        binDTO.setLatitude(bin.getLatitude());
+        binDTO.setSortedWaste(bin.getSortedWaste());
+        binDTO.setUnsortedWaste(bin.getUnsortedWaste());
+        binDTO.setAlertLevel(bin.getAlertLevel());
+
+        return binDTO;
+    }
+
+
+    private Bin fromBinDTOtoBin(BinDTO binDTO) {
+        Bin bin = new Bin();
+
+        bin.setId(binDTO.getId());
+        bin.setLongitude(binDTO.getLongitude());
+        bin.setLatitude(binDTO.getLatitude());
+        bin.setCapacity(binDTO.getCapacity());
+        bin.setSortedWaste(binDTO.getSortedWaste());
+        bin.setUnsortedWaste(binDTO.getUnsortedWaste());
+        bin.setAlertLevel(binDTO.getAlertLevel());
+
+        return bin;
+    }
+}
