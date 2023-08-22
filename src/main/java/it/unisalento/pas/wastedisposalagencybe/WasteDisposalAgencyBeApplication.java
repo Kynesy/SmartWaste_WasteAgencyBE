ce019aba85cd00c651105d0a3a50968be248aae1
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-22 16:19:42 +0200

    first commit

---
+++ src/main/java/it/unisalento/pas/wastedisposalagencybe/WasteDisposalAgencyBeApplication.java
@@ -0,0 +1,13 @@
+package it.unisalento.pas.wastedisposalagencybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteDisposalAgencyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WasteDisposalAgencyBeApplication.class, args);
+    }
+
+}
