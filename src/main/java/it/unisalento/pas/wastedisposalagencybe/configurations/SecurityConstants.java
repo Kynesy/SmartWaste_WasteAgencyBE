120fa15149c5b1b0d5019fd20090d99704f771d3
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-28 09:43:57 +0200

    aggiunti ruoli utente e admin del comune per consentire l'accesso

--- src/main/java/it/unisalento/pas/wastedisposalagencybe/configurations/SecurityConstants.java
+++ src/main/java/it/unisalento/pas/wastedisposalagencybe/configurations/SecurityConstants.java
@@ -1,7 +1,9 @@
 package it.unisalento.pas.wastedisposalagencybe.configurations;
 
 public class SecurityConstants {
-    public static final String[] ISSUER_LIST = {"https://smart-city-waste-management.eu.auth0.com/", "test"};
+    public static final String[] ISSUER_LIST = {"https://smart-city-waste-management.eu.auth0.com/"};
 
     public static final String OPERATOR_ROLE_ID = "OPERATOR";
+    public static final String USER_ROLE_ID = "USER";
+    public static final String ADMIN_ROLE_ID = "ADMIN";
 }
